4d0eb162a5ab0894c625a91221fa9b58f332251d
Cong MA <cong.ma@pointsbet.com>
2022-07-23 21:32:47 +1000

    Initial commit

---
+++ main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // First, Rust style is to indent with four spaces, not a tab.
+    // Second, `println!` calls a Rust macro. If it called a function instead, it would be entered as `println` (without the `!`).
+    println!("Hello, world!");
+}
